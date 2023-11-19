9d9a7d57205f66dc2b7e8bde8e4454989fc844e5
Nikolay Karadzhov <nikolayk@flutterint.com>
2023-11-19 15:30:46 +0200

    try the new Deno.cron runner

--- main.ts
+++ main.ts
@@ -1,23 +1,4 @@
-import { today } from './utils.ts'
-import { schedule, tomorrowMorning } from './schedule.ts'
 import { findAndNotify } from './stadium-notifier.ts'
 
-export const run = async () => {
-  if (await alreadRanToday()) {
-    console.log(`Already ran today. Skip`)
-    return
-  }
-  await findAndNotify()
-  await kv.set(['last-run'], today())
-  await schedule(kv, tomorrowMorning())
-}
-
-const alreadRanToday = async (): Promise<boolean> => {
-  const td = today()
-  const lastRun = await kv.get(['last-run'])
-  return lastRun.value === td
-}
-
-const kv = await Deno.openKv()
-await schedule(kv, tomorrowMorning())
-kv.listenQueue(run)
+//everyday at 7 in the morning
+Deno.cron('Find and notify', '0 07 * * *', findAndNotify)
