ea80750eb7ceee26203fce9dde83f887e7b2cbfa
Nikolay Karadzhov <nikolayk@flutterint.com>
2023-12-11 22:50:34 +0200

    drain undelivered kv queue messages
    which were generated while exploring how kv.enqueue works

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
 import { findAndNotify } from './stadium-notifier.ts'
 
+//drain all parasite undelivered queue mesasges
+// those were generated during exploration of kv.enqueue
+const kv = await Deno.openKv();
+kv.listenQueue(() => { console.log('draining parasite undelivered queue message'); });
+
 //everyday at 7 in the morning
 Deno.cron('Find and notify', '0 07 * * *', findAndNotify)
